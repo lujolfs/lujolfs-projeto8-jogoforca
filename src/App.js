1f878f9db55ae24e99f9b9545a6da497f10bd9a0
lujolfs <lucas.j.frank@gmail.com>
2022-09-24 01:35:17 -0300

    Implementação da ggeração de botões para cada letra através de map.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import { useState } from "react"
 import palavras from "./palavras";
 import forca0 from "./assets/forca0.png";
-import forca1 from "./assets/forca1.png";
-import forca2 from "./assets/forca2.png";
-import forca3 from "./assets/forca3.png";
-import forca4 from "./assets/forca4.png";
-import forca5 from "./assets/forca5.png";
-import forca6 from "./assets/forca6.png";
 
 const alfanumerico = Array.from(Array(26)).map((e, i) => i + 65);
 const alfabeto = alfanumerico.map((x) => String.fromCharCode(x));
@@ -17,21 +11,42 @@ const alfabeto = alfanumerico.map((x) => String.fromCharCode(x));
 export default function App() {
 
     return (
-                <div onClick={defineResposta} className="botaoPalavra">
-                    <p>Aqui vai ficar o botão "Escolher palavra".</p>
+        <>
+            <>
+                <div className="botao-palavra">
+                    <button type="button" onClick={inicioJogo} className="botaoPalavra">
+                        Gerar uma nova palavra.
+                    </button>
                 </div>
+            </>
+            <>
+                <div className="botao-letra">
+                    {alfabeto.map((a) => (
+                        <button type="button" key={a}>{a}</button>
+                    ))
+                    }
+                </div>
+            </>
+        </>
     )
 }
 
-function defineResposta() {
-    const palavra = palavras[Math.floor(Math.random() * palavras.length)]
-    console.log(palavra);
-    const palavraArray = [...palavra];
-    console.log(palavraArray);
-    palavraArray.map((letra) => (
-        <li className="dica" key={letra}>
-            <p>Letra _</p>
-        </li>
-    ))
-}
 
+function inicioJogo() {
+    defineResposta();
+    function defineResposta() {
+        const palavra = palavras[Math.floor(Math.random() * palavras.length)]
+        console.log(palavra);
+        const palavraArray = [...palavra];
+        renderDica();
+
+        function renderDica() {
+            palavraArray.map((letra) => (
+                <li className="dica" key={letra}>
+                    <p>_</p>
+                </li>
+            ))
+
+        }
+    }
+}
