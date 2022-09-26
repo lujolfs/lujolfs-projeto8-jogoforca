0f64db86bcb2c0730bc4b38d5a867a3756c11a57
lujolfs <lucas.j.frank@gmail.com>
2022-09-26 16:37:36 -0300

    Versão para entrega do trabalho.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import palavras from "./palavras";
-import "./styles.css";
+import styled from "styled-components";
+import GlobalStyle from "./GlobalStyle"
+
 
 const alfanumerico = Array.from(Array(26)).map((e, i) => i + 65);
 const alfabeto = alfanumerico.map((x) => String.fromCharCode(x));
 const alfabetoDisabled = {A: true, B: true, C: true, D: true, E: true, F: true, G: true, H: true, I: true, J: true, K: true, L: true, M: true, N: true, O: true, P: true, Q: true, R: true, S: true, T: true, U: true, V: true, W: true, X: true, Y: true, Z:true};
-
 
 export default function App() {
 
@@ -36,7 +37,6 @@ export default function App() {
         setBotaoDesliga(false);
         setDisableList({});
         setChute("");
-        console.log(palavraNormalizadaArray);
     }
 
     function compararLetra(letra) {
@@ -46,13 +46,10 @@ export default function App() {
         if (letrasNormalizadas.includes(letra)) {
             letraCerta.push(letra);
             setLetraCerta(letraCerta);
-            console.log(letraCerta);
-            /* revelaLetra(letra); */
         } else {
             letraErrada.push(letra);
             setLetraErrada(letraErrada);
             setContaForca(contaForca + 1);
-            console.log("Errouuuuu!!");
         }
         verificaAcerto();
     }
@@ -73,7 +70,6 @@ export default function App() {
     }
 
     function verificaAcerto() {
-        console.log(letrasClicadas);
         if (letraCerta.length === letrasNormalizadas.length) {
             venceuJogo();
         } else if (letraErrada.length === 6) {
@@ -87,13 +83,12 @@ export default function App() {
     }
 
     function perdeuJogo() {
+        setContaForca(6);
         alert("Não foi dessa vez! Tente de novo!");
         setDisableList(alfabetoDisabled);
-        setContaForca(6);
     }
 
     function chuteTeste() {
-        console.log(chute);
         setBotaoDesliga(!botaoDesliga);
         setDisableList(alfabetoDisabled);
         const chuteMaiusculo = chute.toUpperCase();
@@ -103,7 +98,6 @@ export default function App() {
         const chuteLetraUnica = Array.from(chuteLetraRepetida);
         setLetrasClicadas(chuteLetraUnica);
         compararLetraChute(chuteLetraUnica);
-        console.log(chuteLetraUnica);
     }
 
     function incluiLetra(letra) {
@@ -116,65 +110,181 @@ export default function App() {
         }
     }
 
-/*     function revelaLetra(letra) {
-        const letraRevelada = palavraLetraRepetida.filter(letra, index);
-        consonle.log(letraRevelada);
-
-    } */
-
-
-
-
     return (
-        <>
-            <>
-                <img src={`assets/forca${contaForca}.png`} alt={`Você cometeu ${contaForca} erros.`} />
-            </>
-            <>
+        <Container>
+            <InicioJogo>
+            <Forca>
+                <ForcaImg src={`assets/forca${contaForca}.png`} alt={`Você cometeu ${contaForca} erros.` } data-identifier="game-image" />
+            </Forca>
+            <GerarPalavra>
+            <Inicio>
                 <div className="botao">
-                    <button type="button" onClick={defineResposta} className="botaoPalavra">
-                        Gerar uma nova palavra.
-                    </button>
+                    <BotaoStart data-identifier="choose-word" type="button" onClick={defineResposta} className="botaoPalavra">
+                        Escolher palavra
+                    </BotaoStart>
                 </div>
-            </>
-            <>
+            </Inicio>
+
+            <Dicas>
                 {letrasDica.map((letra, index) => (
                     <div className="dicasCaixa" key={index}>
                         <div
                             className="dicasProper"
                             key={index}
+                            data-identifier="word"
                         >
                             <h1>{incluiLetra(letra)}</h1>
                         </div>
                     </div>
                 ))}
-            </>
-            <>
+            </Dicas>
+            </GerarPalavra>
+            </InicioJogo>
+            <Botoes>
                 <div className="botao-letra">
                     {alfabeto.map((a) => (
-                        <button
+                        <BotaoLetra
                             disabled={disableList[a]}
                             type="button"
                             onClick={() => compararLetra(a)}
-                            key={a}>
+                            key={a}
+                            data-identifier="letter">
                             {a}
-                        </button>
+                        </BotaoLetra>
                     ))
                     }
                 </div>
-            </>
-            <>
-                <div className="input-direto">
-                    <h1>Eu já sei a palavra!</h1><br />
-                    <input
+            </Botoes>
+            <div>
+                <Chute className="input-direto">
+                    <h1>Já sei a palavra!</h1><br />
+                    <ChutaTudo>
+                    <DigitaChute
                         type="text"
                         disabled={botaoDesliga ? "true" : ""}
                         name="chute"
                         onChange={event => setChute(event.target.value)}
+                        data-identifier="type-guess"
                     />
-                    <button disabled={botaoDesliga ? "true" : ""} onClick={() => chuteTeste()}>Chutar</button>
-                </div>
-            </>
-        </>
+                    <BotaoChutar data-identifier="guess-button" disabled={botaoDesliga ? "true" : ""} onClick={() => chuteTeste()}>Chutar</BotaoChutar>
+                    </ChutaTudo>
+                </Chute>
+            </div>
+            <GlobalStyle />
+        </Container>
     )
 }
+
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+gap: 40px;
+align-items:center;
+`
+const InicioJogo = styled.div`
+display: flex;
+flex-direction: row;
+gap: 100px;
+align-items: center;
+`
+
+const Forca = styled.div`
+box-sizing: border-box;
+height: 100%;
+
+`
+
+const ForcaImg = styled.img`
+width: 100%;
+height: 100%;
+object-fit: contain;
+`
+const GerarPalavra = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+height: 300px;
+width: 100%;
+`
+
+const BotaoStart = styled.button`
+background-color: #26ad60;
+border-radius: 4px;
+border: none;
+height: 80px;
+width: 150px;
+font-size: 15px;
+color: white;
+cursor: pointer;
+
+}
+`
+
+const Inicio = styled.div`
+`
+
+const Dicas = styled.div`
+display: flex;
+margin: 0px, 20px;
+justify-content: space-evenly;
+font-size: 35px;
+width: 100%;
+font-family: arial;
+color: black;
+`
+
+const Botoes = styled.div`
+display: flex;
+flex-wrap: wrap;
+font-size: 35px;
+height: 50px;
+width: 715px;
+margin: 5%;
+`
+
+const BotaoLetra = styled.button`
+font-size 25px;
+height: 50px;
+width: 45px;
+border-radius: 4px;
+border: none;
+margin: 5px;
+color: #407094;
+background-color:#e1ebf4;
+cursor: pointer;
+&:disabled {
+    cursor: not-allowed;
+}
+`
+
+const Chute = styled.div`
+font-family: Arial;
+display: flex;
+align-items: center;
+gap: 15px;
+`
+
+const ChutaTudo = styled.div`
+display: flex;
+`
+
+const DigitaChute = styled.input`
+&:disabled {
+    cursor: not-allowed;
+}
+`
+
+const BotaoChutar = styled.button`
+color: #407094;
+background-color:#e1ebf4;
+border-radius: 4px;
+border:none;
+height: 50px;
+width: 80px;
+cursor: pointer;
+&:disabled {
+    cursor: not-allowed;
+}
+`
