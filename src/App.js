752ffca1270a425b7c025b1b3524637136f8c8a4
lujolfs <lucas.j.frank@gmail.com>
2022-09-26 03:37:10 -0300

    Implementação da contagem de erros e consequente mudança da imagem da forca.

--- src/App.js
+++ src/App.js
@@ -9,33 +9,52 @@ export default function App() {
 
     const [letrasDica, setLetrasDica] = useState([]);
     const [letrasClicadas, setLetrasClicadas] = useState([]);
+    const [letraCerta, setLetraCerta] = useState([]);
+    const [letraErrada, setLetraErrada] = useState([]);
+    const [contaForca, setContaForca] = useState(0);
 
     function defineResposta() {
         const palavra = palavras[Math.floor(Math.random() * palavras.length)]
         const palavraArray = [...palavra]
         setLetrasDica(palavraArray);
         setLetrasClicadas([]);
+        setContaForca(0);
         console.log(palavraArray);
     }
 
     function compararLetra(letra) {
         letrasClicadas.push(letra);
         setLetrasClicadas(letrasClicadas);
-        letrasDica.includes(letra) ? console.log("Essa letra está na palavra.") : console.log("ERROU!!");
-        verificaLetra();
+        if (letrasDica.includes(letra)) {
+            letraCerta.push(letra);
+            setLetraCerta(letraCerta);
+            console.log(letraCerta);
+        } else {
+            letraErrada.push(letra);
+            setLetraErrada(letraErrada);
+            setContaForca(contaForca + 1);
+            console.log(contaForca);
+            console.log(letraErrada.length);
+        }
+        verificaAcerto();
     }
 
-    function verificaLetra() {
+    function verificaAcerto() {
         console.log(letrasClicadas);
+        (letraCerta.length === letrasDica.length) ? console.log("Acertou!!") : console.log("Ainda não.");
+        if (letraErrada.length === 6) {
+            console.log("Perdeu")
+        }
     }
 
     return (
         <>
             <>
                 <h1>Imagem da Forca</h1>
+                <img src={`assets/forca${contaForca}.png`} alt={`Você cometeu ${contaForca} erros.`}/>
             </>
             <>
-                <div className="botao-palavra">
+                <div className="botao">
                     <button type="button" onClick={defineResposta} className="botaoPalavra">
                         Gerar uma nova palavra.
                     </button>
@@ -47,14 +66,14 @@ export default function App() {
                         <div
                             className="dicasProper"
                             key={index}
-                            >
+                        >
                             <h1>_</h1>
                         </div>
-                        <div
+                        {/*                         <div
                             className="acertoLetra hidden"
                             key="'l'+{index}">
                             <h1>{letra}</h1>
-                        </div>
+                        </div> */}
                     </div>
                 ))}
             </>
